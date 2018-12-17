feec76048339334dc057dccc28b4c6e13c3d04d2
lmagoncalo <lmagoncalo@hotmail.com>
2018-12-17 16:00:05 +0000

    Update config.py

--- src/config.py
+++ src/config.py
@@ -9,7 +9,7 @@ class Development(object):
     DEBUG = True
     TESTING = False
     JWT_SECRET_KEY = 'hhgaghhgsdhdhdd'
-    SQLALCHEMY_DATABASE_URI = 'postgres://postgres:123456789@127.0.0.1:5432/smartcooking'
+    SQLALCHEMY_DATABASE_URI = 'postgres://jhenbvzzxcjukg:f2f44fb6e0a10677c848586917f80cccae04d488bd03f809ba98398edad3030d@ec2-54-217-237-93.eu-west-1.compute.amazonaws.com:5432/d87uli0648co1c'
 
 class Production(object):
     """
@@ -17,7 +17,7 @@ class Production(object):
     """
     DEBUG = False
     TESTING = False #estava a false
-    SQLALCHEMY_DATABASE_URI = 'postgres://postgres:123456789@127.0.0.1:5432/smartcooking'
+    SQLALCHEMY_DATABASE_URI = 'postgres://jhenbvzzxcjukg:f2f44fb6e0a10677c848586917f80cccae04d488bd03f809ba98398edad3030d@ec2-54-217-237-93.eu-west-1.compute.amazonaws.com:5432/d87uli0648co1c'
     JWT_SECRET_KEY = 'hhgaghhgsdhdhdd'
 
 app_config = {
